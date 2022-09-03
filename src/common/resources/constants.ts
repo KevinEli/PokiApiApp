4f84ae13924c38d78cf544d5b4f5aa85b8c3c5e2
KevinEli <kevindermo@hotmail.com>
2022-09-02 23:15:11 -0600

    - get 10 rows of pokemon external API

--- src/common/resources/constants.ts
+++ src/common/resources/constants.ts
@@ -1,6 +1,6 @@
 export const Constants = Object.freeze({
   ExternalUrl: {
-    GetPokemon: 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=2',
+    GetPokemon: 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10',
     GetPokemonType: 'https://pokeapi.co/api/v2/type/'
   },
   TableName: 'PokemonLocal'
